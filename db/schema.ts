4173bc581a4c143c81819ab5d7850a49aa68d831
christufur <chrismeraz1@gmail.com>
2026-04-17 22:58:44 -0600

    feat: initial commit

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+// Add your tables here. Example:
+// export const users = sqliteTable("users", {
+//   id: int("id").primaryKey({ autoIncrement: true }),
+//   name: text("name").notNull(),
+//   createdAt: int("created_at", { mode: "timestamp" }).notNull().default(sql`(unixepoch())`),
+// });
